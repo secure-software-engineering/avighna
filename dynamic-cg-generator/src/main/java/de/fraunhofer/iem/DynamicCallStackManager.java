65948255b0339dae891a19156bff20fb47f547c6
Ranjith K <ranjith.masthikatte@gmail.com>
2022-02-13 03:48:07 +0100

    Update DynamicCallStackManager.java

--- dynamic-cg-generator/src/main/java/de/fraunhofer/iem/DynamicCallStackManager.java
+++ dynamic-cg-generator/src/main/java/de/fraunhofer/iem/DynamicCallStackManager.java
@@ -2,18 +2,24 @@ package de.fraunhofer.iem;
 
 import de.fraunhofer.iem.util.LoggerUtil;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.logging.Level;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
 
+/**
+ * Manager to mange the Dynamic call stack, this will be instrumented in the application code to generate Dynamic CG
+ *
+ * @author Ranjith Krishnamurthy
+ */
 public class DynamicCallStackManager {
     private static final List<DynamicCallStack> myDynamicCallStack = Collections.synchronizedList(new ArrayList<>());
 
+    /**
+     * Returns the Dynamic CG of the given thread if present otherwise returns null
+     *
+     * @return Dynamic CG if present otherwise returns null
+     */
     private static DynamicCallStack getDynamicCallStack() {
         long id = Thread.currentThread().getId();
 
@@ -26,6 +32,12 @@ public class DynamicCallStackManager {
         return null;
     }
 
+    /**
+     * Method call of the application/library code
+     *
+     * @param methodSignature Method signature
+     * @param isLibraryCall   Is the method a library code
+     */
     public static void methodCall(String methodSignature, boolean isLibraryCall) {
         DynamicCallStack dynamicCallStack = getDynamicCallStack();
 
@@ -34,8 +46,7 @@ public class DynamicCallStackManager {
 
             dynamicCallStack = new DynamicCallStack(Thread.currentThread().getId());
             myDynamicCallStack.add(dynamicCallStack);
-            LoggerUtil.LOGGER.log(Level.INFO, "ENTERED");
-            System.out.println(System.identityHashCode("----> " + DynamicCallStackManager.class.getClassLoader()));
+            LoggerUtil.LOGGER.info("Created new Dynamic Call stack for the process id = " + Thread.currentThread().getId());
         }
 
         if (isLibraryCall) {
@@ -45,6 +56,12 @@ public class DynamicCallStackManager {
         }
     }
 
+    /**
+     * Method return call of the application/library code
+     *
+     * @param methodSignature Method signature
+     * @param isLibraryCall   Is the method a library code
+     */
     public static void methodReturn(String methodSignature, boolean isLibraryCall) {
         DynamicCallStack dynamicCallStack = getDynamicCallStack();
 
@@ -52,6 +69,8 @@ public class DynamicCallStackManager {
             if (isLibraryCall) return;
 
             dynamicCallStack = new DynamicCallStack(Thread.currentThread().getId());
+            LoggerUtil.LOGGER.log(Level.WARNING, "Created new Dynamic Call stack for the process id = " + Thread.currentThread().getId() +
+                    "However, it happened in methodReturn, Please check the logic.");
         }
 
         if (isLibraryCall) {
@@ -61,12 +80,17 @@ public class DynamicCallStackManager {
         }
     }
 
+    /**
+     * API that allows user to forcefully write the Dynamic CG to the file
+     */
     public static void writeForcefully() {
         DynamicCallStack dynamicCallStack = getDynamicCallStack();
 
         if (dynamicCallStack == null) {
             dynamicCallStack = new DynamicCallStack(Thread.currentThread().getId());
             myDynamicCallStack.add(dynamicCallStack);
+            LoggerUtil.LOGGER.log(Level.WARNING, "Created new Dynamic Call stack for the process id = " + Thread.currentThread().getId() +
+                    "However, it happened in writeForcefully, Please check the logic.");
         }
 
         dynamicCallStack.writeRequest();
