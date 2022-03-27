7fee73c8d32f7f64537fe7c7b8852b1f9a3a5f35
Ranjith K <ranjith.masthikatte@gmail.com>
2022-03-27 05:08:32 +0200

    generates image file from the dot graph

---
+++ dynamic-cg-cmd-interface/src/main/java/de/fraunhofer/iem/util/DotToImgUtil.java
@@ -0,0 +1,70 @@
+package de.fraunhofer.iem.util;
+
+import de.fraunhofer.iem.MainInterface;
+import guru.nidi.graphviz.engine.Format;
+import guru.nidi.graphviz.engine.Graphviz;
+import org.apache.commons.cli.CommandLine;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Utility class to convert the DOT file to image file
+ *
+ * @author Ranjith Krishnamurthy
+ */
+public class DotToImgUtil {
+    /**
+     * This method converts the given dot file into image file
+     *
+     * @param dotFileName         Dot file name
+     * @param outputImageFileName Image file name
+     */
+    private static void saveDotAsSVG(String dotFileName, String outputImageFileName) {
+        try {
+            Graphviz.fromFile(new File(dotFileName)).render(Format.SVG).toFile(new File(outputImageFileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Generates images files for all the DOT files present in the given output root directory
+     */
+    public static void generateImageFromDot() {
+        CommandLine commandLine = CommandLineUtility.getCommandLine();
+
+        List<String> dotFiles = new ArrayList<>();
+
+        try {
+            Files.walk(Paths.get(CommandLineUtility.getCommandLine().getOptionValue(CommandLineUtility.OUT_ROOT_DIR_SHORT)
+                            + File.separator + "allDotFiles" + File.separator))
+                    .filter(Files::isRegularFile)
+                    .filter(it -> it.toAbsolutePath().toString().endsWith(".dot"))
+                    .forEach(it -> {
+                        dotFiles.add(it.toAbsolutePath().toString());
+                    });
+
+            for (String dotFileName : dotFiles) {
+                saveDotAsSVG(dotFileName, dotFileName.substring(0, dotFileName.lastIndexOf('.')) + ".svg");
+            }
+
+            if (!commandLine.hasOption(CommandLineUtility.SAVE_DOT_FILE_LONG)) {
+                for (String dotFileName : dotFiles) {
+                    saveDotAsSVG(dotFileName, dotFileName.substring(0, dotFileName.lastIndexOf('.')) + ".svg");
+                    new File(dotFileName).delete();
+                }
+            } else {
+                for (String dotFileName : dotFiles) {
+                    saveDotAsSVG(dotFileName, dotFileName.substring(0, dotFileName.lastIndexOf('.')) + ".svg");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
