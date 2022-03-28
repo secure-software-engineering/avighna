90ea7585608e6ed85d727e39a16aed9c29dbcc64
Ranjith K <ranjith.masthikatte@gmail.com>
2022-03-28 23:06:57 +0200

    added test cases

---
+++ hybridCG-merger/src/test/java/de/fraunhofer/iem/hybridCG/FilesUtils.java
@@ -0,0 +1,43 @@
+package de.fraunhofer.iem.hybridCG;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Utility class for file processing
+ *
+ * @author Ranjith Krishnamurthy
+ */
+public class FilesUtils {
+    /**
+     * Process the given app path that contains the classes and returns the list of classes.
+     *
+     * @param appClassesPath App class path that contains the classes to be converted into Jimple
+     * @return List of classes name
+     */
+    protected static List<String> getClassesAsList(String appClassesPath) {
+        Path path = Paths.get(appClassesPath);
+
+        List<String> appClasses = new ArrayList<>();
+
+        try {
+            Files.find(path,
+                    Integer.MAX_VALUE,
+                    (filePath, fileAttr) -> filePath.toString().endsWith(".class") && fileAttr.isRegularFile()
+            ).forEach(p -> appClasses.add(p.toString()
+                    .replace(path.toString(), "")
+                    .replaceAll("\\\\", ".")
+                    .replaceAll("^\\.", "")
+                    .replaceAll("\\.class$", "")));
+        } catch (IOException e) {
+            System.err.println("Something went wrong!\nStacktrace: \n");
+            e.printStackTrace();
+        }
+
+        return appClasses;
+    }
+}
---
+++ hybridCG-merger/src/test/java/de/fraunhofer/iem/hybridCG/TestTheLibrary.java
@@ -0,0 +1,68 @@
+package de.fraunhofer.iem.hybridCG;
+
+import de.fraunhofer.iem.exception.DotToImgException;
+import de.fraunhofer.iem.exception.DtsSerializeUtilException;
+import de.fraunhofer.iem.exception.UnexpectedError;
+import de.fraunhofer.iem.exception.DtsZipUtilException;
+import soot.*;
+import soot.options.Options;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+/**
+ * Test: check it generates valid hybrid call graph
+ *
+ * @author Ranjith Krishnamurthy
+ */
+public class TestTheLibrary {
+    @Test
+    public void test() throws UnexpectedError, DtsSerializeUtilException, DtsZipUtilException, DotToImgException {
+        //TODO: Remove this after testing
+        String appClassPath = "D:\\cgbench\\CGBench\\bean\\target\\classes";
+
+        initializeSoot(appClassPath);
+
+        String dtsFileName = "D:\\cgbench\\CGBench\\bean\\output\\dynamic_cg.dst";
+
+        new HybridCallGraph().merge(dtsFileName, Scene.v().getCallGraph(), "callgraph", "callgraph", ImageType.SVG);
+    }
+
+    /**
+     * Initializes the soot
+     */
+    private static void initializeSoot(String appClassPath) {
+        //TODO: Remove this after testing
+        G.reset();
+        Options.v().set_keep_line_number(true);
+
+        Options.v().setPhaseOption("cg.spark", "on");
+
+        Options.v().setPhaseOption("cg", "all-reachable:true");
+        Options.v().set_allow_phantom_refs(true);
+        Options.v().set_soot_classpath(appClassPath);
+        Options.v().set_prepend_classpath(true);
+        Options.v().set_whole_program(true);
+        Options.v().set_allow_phantom_refs(true);
+
+        Options.v().setPhaseOption("jb", "use-original-names:true");
+        //Options.v().setPhaseOption("jb.lns", "enabled:false");
+
+        Options.v().set_output_format(Options.output_format_none);
+
+        List<String> appClasses = new ArrayList<>(FilesUtils.getClassesAsList(appClassPath));
+
+        List<SootMethod> entries = new ArrayList<SootMethod>();
+        for (String appClass : appClasses) {
+            SootClass sootClass = Scene.v().forceResolve(appClass, SootClass.BODIES);
+            sootClass.setApplicationClass();
+            entries.addAll(sootClass.getMethods());
+        }
+
+        Scene.v().setEntryPoints(entries);
+        Scene.v().loadNecessaryClasses();
+        PackManager.v().getPack("cg").apply();
+    }
+}
