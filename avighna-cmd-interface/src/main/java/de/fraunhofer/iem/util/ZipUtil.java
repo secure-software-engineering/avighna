0639f3024b9468fd72fbcc64ebcb3b8571443058
Ranjith K <ranjith.masthikatte@gmail.com>
2022-12-29 18:46:07 +0100

    using library to zip and unzip DST files

--- avighna-cmd-interface/src/main/java/de/fraunhofer/iem/util/ZipUtil.java
+++ avighna-cmd-interface/src/main/java/de/fraunhofer/iem/util/ZipUtil.java
@@ -1,15 +1,13 @@
 package de.fraunhofer.iem.util;
 
+import net.lingala.zip4j.ZipFile;
 import org.apache.commons.cli.CommandLine;
-import org.apache.commons.logging.Log;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Objects;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 
 public class ZipUtil {
     public static void generateDst(CommandLine commandLine) {
@@ -20,64 +18,19 @@ public class ZipUtil {
                     .filter(it -> it.toAbsolutePath().toString().endsWith(".ser"))
                     .forEach(it -> {dotFiles.add(it.toAbsolutePath().toString());});
 
-            FileOutputStream fos = new FileOutputStream(CommandLineUtility.getCommandLine().getOptionValue(CommandLineUtility.OUT_ROOT_DIR_SHORT) + File.separator + "dynamic_cg.dst");
-
-            ZipOutputStream zos = new ZipOutputStream(fos);
+            ZipFile zipFile = new ZipFile(CommandLineUtility.getCommandLine().getOptionValue(CommandLineUtility.OUT_ROOT_DIR_SHORT) + File.separator + "dynamic_cg.dst");
 
             for (String path : dotFiles) {
-                zipFile(path, zos, "");
+                zipFile.addFile(new File(path));
             }
 
-            zipFolder(CommandLineUtility.getCommandLine().getOptionValue(CommandLineUtility.OUT_ROOT_DIR_SHORT) +
+            zipFile.addFolder(new File(CommandLineUtility.getCommandLine().getOptionValue(CommandLineUtility.OUT_ROOT_DIR_SHORT) +
                     File.separator +
-                    "dynamicCP" + File.separator, "dynamicCP", zos);
+                    "dynamicCP" + File.separator));
 
-            zos.close();
-            fos.close();
+            zipFile.close();
         } catch (IOException e) {
             LoggerUtil.getLOGGER().warning("Something went wrong while generating DST file = " + e.getMessage());
-        }
-    }
-
-    private static void zipFile(String path, ZipOutputStream zos, String rootDir) throws IOException {
-        File temp = new File(path);
-        FileInputStream fileInputStream = new FileInputStream(temp);
-        String zipEntryName;
-
-        if (Objects.equals(rootDir, "")) {
-            zipEntryName = temp.getName();
-        } else {
-            zipEntryName = rootDir + "/" + temp.getName();
-        }
-
-        ZipEntry zipEntry = new ZipEntry(zipEntryName);
-        zos.putNextEntry(zipEntry);
-        byte[] bytes = new byte[1024];
-        int length;
-        while((length = fileInputStream.read(bytes)) >= 0) {
-            zos.write(bytes, 0, length);
-        }
-        fileInputStream.close();
-    }
-
-    private static void zipFolder(String folder, String parentFolder, ZipOutputStream zos) throws IOException {
-        File dirFile = new File(folder);
-
-        if (!dirFile.exists()) {
-            return;
-        }
-
-        File[] files = dirFile.listFiles();
-
-        if (files == null) return;
-
-        for (File file : files) {
-            if (file.isDirectory()) {
-                zipFolder(file.getAbsolutePath(), parentFolder + "/" + file.getName(), zos);
-                continue;
-            }
-
-            zipFile(file.getAbsolutePath(), zos, parentFolder);
         }
     }
 }
--- avighna-merger/src/main/java/de/fraunhofer/iem/util/ZipUtility.java
+++ avighna-merger/src/main/java/de/fraunhofer/iem/util/ZipUtility.java
@@ -1,20 +1,14 @@
 package de.fraunhofer.iem.util;
 
 import de.fraunhofer.iem.exception.DtsZipUtilException;
+import net.lingala.zip4j.ZipFile;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 
 /**
- * Taken from https://www.journaldev.com/960/java-unzip-file-example#:~:text=To%20unzip%20a%20zip%20file,present%20in%20the%20zip%20file.
+ * @author Ranjith Krishnamurthy
  */
 public class ZipUtility {
     /**
@@ -36,35 +30,10 @@ public class ZipUtility {
         if (!dir.exists())
             dir.mkdirs();
 
-        try {
-            FileInputStream fileInputStream = new FileInputStream(dtsFilePath);
-            ZipInputStream zipInputStream = new ZipInputStream(fileInputStream);
-            ZipEntry zipEntry = zipInputStream.getNextEntry();
-
-            byte[] buffer = new byte[1024];
-
-            while (zipEntry != null) {
-                String fileName = zipEntry.getName();
-                File newFile = new File(dir.getAbsolutePath() + File.separator + fileName);
-
-                new File(newFile.getParent()).mkdirs();
-
-                FileOutputStream fos = new FileOutputStream(newFile);
-                int len;
-                while ((len = zipInputStream.read(buffer)) > 0) {
-                    fos.write(buffer, 0, len);
-                }
-                fos.close();
-
-                zipInputStream.closeEntry();
-                zipEntry = zipInputStream.getNextEntry();
-            }
-
-            zipInputStream.closeEntry();
-            zipInputStream.close();
-            fileInputStream.close();
+        try (ZipFile zipFile = new ZipFile(new File(dtsFilePath))) {
+            zipFile.extractAll(dir.getAbsolutePath());
         } catch (IOException e) {
-            throw new DtsZipUtilException("Failed to unzip DTS file." +
+            throw new DtsZipUtilException("Failed to extract provided DST file." +
                     "\nMessage = " + e.getMessage());
         }
 
